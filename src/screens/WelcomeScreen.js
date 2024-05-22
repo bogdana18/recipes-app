e376f7258f94d98fdf7647ba9a4d4101d5846eb1
Usenko Bohdana <usenkobogdana19@gmail.com>
2024-05-22 23:00:17 +0300

    fix: welcome page

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,20 +1,46 @@
 import { View, Text, Image } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { useNavigation } from '@react-navigation/native';
+import tw from 'twrnc';
 
 export default function WelcomeScreen() {
+
+  const ring1padding = useSharedValue(0);
+  const ring2padding = useSharedValue(0);
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    ring1padding.value = 0;
+    ring2padding.value = 0;
+    setTimeout(() => ring1padding.value = withSpring(ring1padding.value + hp(5)), 100);
+    setTimeout(() => ring2padding.value = withSpring(ring2padding.value + hp(5.5)), 300);
+
+    setTimeout(() => navigation.navigate('Home'), 2500);
+  }, []);
+
   return (
-    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+    <View style={tw`flex-1 justify-center items-center space-y-10 bg-amber-500`}>
       <StatusBar style="light" />
 
-      {/*logo image*/}
-      <View className="bg-white/20 rounded-full p-10">
-        <View className="bg-white/20 rounded-full p-8">
-          <Image soutce={require('../../assets/fried-chicken-meal-svgrepo-com.svg')}
-            style={{width: 200, height: 200}} />
-        </View>
+      <Animated.View style={[tw`bg-white/20 rounded-full`, { padding: ring2padding }]}>
+        <Animated.View style={[tw`bg-white/20 rounded-full`, { padding: ring1padding }]}>
+          <Image source={require('../../assets/fried-chicken-meal-svgrepo-com.png')}
+            style={{ width: hp(20), height: hp(20) }} />
+        </Animated.View>
+      </Animated.View>
+
+      <View style={tw`flex items-center space-y-2`}>
+        <Text style={[tw`font-bold text-white tracking-widest`, { fontSize: hp(7) }]}>
+          Foody
+        </Text>
+        <Text style={[tw`font-medium text-white tracking-widest`, { padding: hp(2) }]}>
+          Food is always right
+        </Text>
       </View>
-      {/*Title*/}
     </View>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/screens/WelcomeScreen.js"],
+  content: ["./App.{js,jsx,ts,tsx}",
+  "./screens/**/*.{js,jsx,ts,tsx}",  "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
